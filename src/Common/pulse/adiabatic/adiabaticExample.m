030a15a275fc31506d32ccce1d9ba1ee520d2574
Amie Demmans <demmansa@mcmaster.ca>
2024-03-05 16:04:37 -0500

    Cleaning up example script

--- src/Common/pulse/adiabatic/adiabaticExample.m
+++ src/Common/pulse/adiabatic/adiabaticExample.m
@@ -1,8 +1,14 @@
-%% Example  script Hsn
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Hyperbolic Secant 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Define Initial Parameters 
 Params.B0 = 3;
 Params.TissueType = 'WM';
 Params = DefaultCortexTissueParams(Params);
+Params.NumPools = 1; % or 2
 
+% Define Hyperbolic Secant Parameter
 Params.Inv.PulseOpt.beta = 672;  
 Params.Inv.PulseOpt.n = 1; 
 Params.Inv.PulseOpt.mu = 5;
@@ -10,28 +16,27 @@ Params.Inv.PulseOpt.A0 = 13.726;
 Params.Inv.nSamples = 512;
 Params.Inv.Trf = 10.24/1000;
 Params.Inv.shape = 'hsn';
-%Params.Inv.shape = 'lorentz';
-Params.NumPools = 1; % or 2
 
-%% Inversion
+
+% Apply Inversion pulse by calling GetAdiabatic
 Params.dispFigure = 0;
 [inv_pulse, ~] = GetAdiabaticPulse( Params.Inv.Trf, Params.Inv.shape, ...
                                     Params.Inv);
 
-% Check plot -> function for only plotting pulse (1 pool)
+% To check your pulse: Plot  
 t = linspace(0, Params.Inv.Trf, Params.Inv.nSamples);
-    figure; tiledlayout(1,2)
-    nexttile; plot(t*1000, abs(inv_pulse), 'LineWidth', 3); 
-    xlabel('Time(ms)'); ylabel('B_1 (μT)')
-    title('Amplitude Function');ax = gca; ax.FontSize = 20;
-    
-    nexttile; plot(t*1000, imag(inv_pulse), 'LineWidth', 3);
-    xlabel('Time(ms)'); ylabel('Frequency (Hz)');
-    title('Frequency Modulation function');ax = gca; ax.FontSize = 20;
-set(gcf,'Position',[100 100 1200 500])
+PlotPulse(t, inv_pulse);
 
 % bloch sim and return magnetization
-BlochSimCallFunction
+Params.PulseOpt = defaultHyperbolicSecParams(Params);
 
-M_t = blochSimAdiabaticPulse( inv_pulse, Params.Inv ,...
-                                  0, Params, M_t, []);
+[rf_pulse, A_t, omega1] = hyperbolicSecant_pulse(Params.Inv.Trf, Params.Inv);
+BlochSimCallFunction(Params, rf_pulse, t, A_t, omega1);
+
+% 
+% M_t = blochSimAdiabaticPulse( inv_pulse, Params.Inv ,...
+%                                   0, Params, M_t, []);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Lorentz
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
