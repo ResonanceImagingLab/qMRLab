620341d08c62f8e39dbe1491755a8cdde58dace7
Amie Demmans <demmansa@mcmaster.ca>
2024-03-11 17:09:54 -0400

    Add Hanning Pulse

--- src/Common/pulse/adiabatic/adiabaticExample.m
+++ src/Common/pulse/adiabatic/adiabaticExample.m
@@ -77,12 +77,12 @@ Params.Ra = 1; % 1000ms
 Params.NumPools = 1;
 
 % Define Gaussian Parameters
-Params.Inv.PulseOpt.beta = 300;  
+Params.Inv.PulseOpt.beta = 50;  
 Params.Inv.PulseOpt.A0 = 13.726;
-Params.Inv.PulseOpt.Q = 4;
+%Params.Inv.PulseOpt.Q = 4;
 Params.Inv.nSamples = 512;
-Params.Inv.Trf = 20/1000;
-Params.Inv.shape = 'gaussc';
+Params.Inv.Trf = 10/1000;
+Params.Inv.shape = 'gauss';
 
 % Apply inversion pulse by calling GetAdiabatic 
 Params.dispFigure = 0;
@@ -94,6 +94,32 @@ t = linspace(0, Params.Inv.Trf, Params.Inv.nSamples);
 PlotPulse(t, inv_pulse);
 
 % Plot Bloch Sim Results based on NumPools 
-[rf_pulse, omega1, A_t, ~] = GaussC_pulse(Params.Inv.Trf, Params.Inv);
+[rf_pulse, omega1, A_t, ~] = Gauss_pulse(Params.Inv.Trf, Params.Inv);
 BlochSimCallFunction(inv_pulse, t, A_t, omega1, Params);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Hanning 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Define Initial Parameters
+Params.B0 = 3;
+Params.TissueType = 'WM';
+Params = DefaultCortexTissueParams(Params);
+Params.R2a = 80/1000; % 80 ms
+Params.Ra = 1; % 1000ms
+Params.NumPools = 1;
+
+% Define Gaussian Parameters 
+Params.Inv.PulseOpt.A0 = 13.726;
+Params.Inv.nSamples = 512;
+Params.Inv.Trf = 10/1000;
+Params.Inv.shape = 'hanning';
+
+% Apply inversion pulse by calling GetAdiabatic 
+Params.dispFigure = 0;
+[inv_pulse, ~] = GetAdiabaticPulse( Params.Inv.Trf, Params.Inv.shape, ...
+                                    Params.Inv);
+
+% To check your pulse: Plot  
+t = linspace(0, Params.Inv.Trf, Params.Inv.nSamples);
+PlotPulse(t, inv_pulse);
 
