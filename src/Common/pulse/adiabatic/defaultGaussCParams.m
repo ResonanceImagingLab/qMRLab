abd31f674cbe06a81db61aa72535803f6a0c4c6b
Amie Demmans <demmansa@mcmaster.ca>
2024-03-11 15:15:53 -0400

    Create default Gauss Params

---
+++ src/Common/pulse/adiabatic/defaultGaussCParams.m
@@ -0,0 +1,26 @@
+function PulseOpt = defaultGaussCParams(PulseOpt)
+
+% Function designed to be used with the adiabatic pulse code
+% Fills in default values if they are not user-defined
+
+if(~isfield(PulseOpt,'beta') || isempty(PulseOpt.beta) || ~isfinite(PulseOpt.beta))
+    % Default beta value in rad/s
+    PulseOpt.beta = 50;       
+end
+
+if(~isfield(PulseOpt,'A0') || isempty(PulseOpt.A0) || ~isfinite(PulseOpt.A0))
+    % Peak B1 of the pulse in microTesla
+    PulseOpt.A0 = 17.5;       
+end
+
+if(~isfield(PulseOpt,'nSamples') || isempty(PulseOpt.nSamples) || ~isfinite(PulseOpt.nSamples))
+    % Peak B1 of the pulse in microTesla
+    PulseOpt.nSamples = 512;       
+end
+
+% if(~isfield(PulseOpt,'Q') || isempty(PulseOpt.Q) || ~isfinite(PulseOpt.Q))
+%     % Peak B1 of the pulse in microTesla
+%     PulseOpt.Q = 4;       
+% end
+
+return;
