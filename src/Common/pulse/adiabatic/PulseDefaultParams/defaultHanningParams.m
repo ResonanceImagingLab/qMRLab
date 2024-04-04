da961ba4e5b30874dece138038bfe7145bfef170
Amie Demmans <demmansa@mcmaster.ca>
2024-04-03 21:14:18 -0400

    Changed params to match adiabaticexample

--- src/Common/pulse/adiabatic/PulseDefaultParams/defaultHanningParams.m
+++ src/Common/pulse/adiabatic/PulseDefaultParams/defaultHanningParams.m
@@ -3,6 +3,10 @@ function PulseOpt = defaultHanningParams(PulseOpt)
 % Function designed to be used with the adiabatic pulse code
 % Fills in default values if they are not user-defined
 
+if(~isfield(PulseOpt,'beta') || isempty(PulseOpt.beta) || ~isfinite(PulseOpt.beta))
+    % Default beta value in rad/s (modulation angular frequency)
+    PulseOpt.beta = 175;       
+end
 
 if(~isfield(PulseOpt,'A0') || isempty(PulseOpt.A0) || ~isfinite(PulseOpt.A0))
     % Peak B1 of the pulse in microTesla
@@ -10,9 +14,13 @@ if(~isfield(PulseOpt,'A0') || isempty(PulseOpt.A0) || ~isfinite(PulseOpt.A0))
 end
 
 if(~isfield(PulseOpt,'nSamples') || isempty(PulseOpt.nSamples) || ~isfinite(PulseOpt.nSamples))
-    % Peak B1 of the pulse in microTesla
+    % Default number of samples taken based on machine properties
     PulseOpt.nSamples = 512;       
 end
 
+if(~isfield(PulseOpt,'Q') || isempty(PulseOpt.Q) || ~isfinite(PulseOpt.Q))
+    % Adiabaticity factor 
+    PulseOpt.Q = 4.2e-4;       
+end
 
 return;
