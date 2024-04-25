c17ad713afaa4542777b17da650cf827fe0c692e
Amie Demmans <demmansa@mcmaster.ca>
2024-04-25 12:18:20 -0400

    Update to work with GUI

--- src/Addons/SimAdiabatic/blochSimFunctions/plotAdiabaticPulse.m
+++ src/Addons/SimAdiabatic/blochSimFunctions/plotAdiabaticPulse.m
@@ -1,4 +1,4 @@
-function plotAdiabaticPulse(t, inv_pulse, Params)
+function plotAdiabaticPulse(ax1,ax2, t, inv_pulse, Params)
 
 
 %% Adiabatic Pulse Plot function 
@@ -13,30 +13,29 @@ function plotAdiabaticPulse(t, inv_pulse, Params)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 fs = 20; 
-figure; 
-tiledlayout(1,2)
+%figure; 
 
 % Plot Amplitude Function
-    nexttile;
-    plot(t*1000, abs(inv_pulse), 'LineWidth', 3); 
-    xlabel('Time(ms)'); 
-    ylabel('B_1 (μT)')
-    title('Amplitude Function','FontWeight','normal');
-    ax = gca; 
-    ax.FontSize = fs;
+    %ax1 = axes(adiabaticPulse);
+    plot(ax1, t*1000, abs(inv_pulse), 'LineWidth', 3); 
+    xlabel(ax1, 'Time(ms)'); 
+    ylabel(ax1, 'B_1 (μT)')
+    title(ax1,'Amplitude Function','FontWeight','normal');
+    ax1.FontSize = fs;
+    %set(gca, 'FontSize', fs);
 
 % Plot Frequency Modulation Function
-    
-    nexttile; 
-    plot(t*1000, imag(inv_pulse), 'LineWidth', 3);
-    xlabel('Time(ms)'); 
-    ylabel('Frequency (Hz)');
-    title('Frequency Modulation function','FontWeight','normal');
-    ax = gca; 
-    ax.FontSize = fs;
+    %ax2 = axes(adiabaticPulse);
+    %subplot(adiabaticPulse, 1,2,2); 
+    plot(ax2, t*1000, imag(inv_pulse), 'LineWidth', 3);
+    xlabel(ax2, 'Time(ms)'); 
+    ylabel(ax2,'Frequency (Hz)');
+    title(ax2,'Frequency Modulation function','FontWeight','normal');
+    ax2.FontSize = fs;
+    %set(gca, 'FontSize', fs);
 
 % Scale display size     
-set(gcf,'Position',[100 100 1200 500])
+%set(gcf,'Position',[100 100 1200 500])
 
 % Set title of pulse 
-sgtitle(Params.Inv.shape, 'FontSize', fs+4,'FontWeight','bold')
+%sgtitle(ax1, ax2, Params.Inv.shape, 'FontSize', fs+4,'FontWeight','bold')
