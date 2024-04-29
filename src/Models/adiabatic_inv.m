a0b9f385d4fdc14ac2351d0266ef9aae8939fd06
Amie Demmans <demmansa@mcmaster.ca>
2024-04-29 14:48:19 -0400

    Added boxes to fill default tissue params

--- src/Models/adiabatic_inv.m
+++ src/Models/adiabatic_inv.m
@@ -1,25 +1,73 @@
-classdef adiabatic_inv < AbstractModel
-
+classdef adiabatic_inv < AbstractModel 
         properties
-        MRIinputs = {'PulseParams', 'DefaultTissueParams'}; % No input data needed 
-        xnames = {}; % Not sure what there are for but dont think needed 
+        MRIinputs = {}; % No data needs to be downloaded
+        xnames = {}; % Box names for Fitting section which I am not using  
         voxelwise = 0; % No voxel wise fitting 
 
+        %X = AI_defaultTissueParams;
         % Creates sections in protocol boxes. Fill default vals later 
-        Prot = struct('PulseParams', struct('Format',{{'beta(rad/s)'  'A0' 'n' 'nSamples' 'Q' 'Trf'}} ...
+        Prot = struct('PulseParams', struct('Format',{{'beta(rad/s)' ; 'A0'; 'n' ;'nSamples' ;'Q' ;'Trf'}} ...
             ,'Mat',[]), ...
-            'DefaultTissueParams', struct('Format',{{}},'Mat',[]));
+            'DefaultTissueParams', struct('Format',{{'M0a'; 'Raobs'; 'R'; 'T2a'; 'T1D'; 'lineshape'; 'R1b'; 'T2b'; 'Ra'; 'M0b'; 'D'}},'Mat', []));
 
-                          
-        buttons = {};
+        % Prot = struct('DefaultTissueParams', struct('Format',{{'B0'; 'TissueType'; 'M0a'; 'Raobs'; 'R'; 'T2a'; 'T1D'; 'lineshape'; 'R1b'; 'T2b'; 'Ra'; 'M0b'; 'D'}},'Mat', []));      
+
+        buttons = {'TissueType', {'GM', 'WM'},...
+            'B0', {'3', '7', '1.5'}, ...
+            'NumPools (1 or 2)', 1, ...
+            'PANEL','PlotAdiabatic',1, 'Yes',false,...
+            'PANEL', 'BlochSim 1 Pool', 1, 'Yes', true, ... 
+            'PANEL', 'BlochSim 2 pool', 1, 'Yes', false};
         options= struct();
-         
-end 
-methods 
+        TissueParams = [];
+        end 
 
+
+methods (Hidden=true)
+% Hidden methods goes here.
+end
+
+
+methods 
 
 function obj = adiabatic_inv()
             obj.options = button2opts(obj.buttons);
-        end
+            obj = UpdateFields(obj);
+
+end
+
+function obj = UpdateFields(obj)
+    obj.TissueParams = AI_defaultTissueParams(str2double(obj.options.B0),obj.options.TissueType);
+    obj.Prot.DefaultTissueParams.Mat = obj.TissueParams;
+    % B0 = str2double(obj.options.B0);
+    % TissueType = obj.options.TissueType;
+    % Params = struct();
+    % Params.B0 = B0;
+    % Params.TissueType = TissueType;
+    % Params = AI_defaultTissueParams(Params);
+    % obj.Prot.DefaultTissueParams.Mat = [TissueType, num2str(B0), num2str(Params.M0a), num2str(Params.Raobs), num2str(Params.R), ...
+    % num2str(Params.T2a), num2str(Params.T1D), Params.lineshape, num2str(Params.R1b), num2str(Params.T2b), num2str(Params.Ra),...
+    % num2str(Params.M0b), num2str(Params.D)];
+
+
+end
+
 end 
 end 
+
+
+
+            % paramNames = {'B0'; 'TissueType'; 'M0a'; 'Raobs'; 'R'; 'T2a'; 'T1D'; 'lineshape'; 'R1b'; 'T2b'; 'Ra'; 'M0b'; 'D'};
+            % paramValues = { 3 ;    {'GM'};      [];     [];    [];   [];    [];       [];      [];     [];     [];  [];   []};
+            % Params = table(paramValues{:}, 'VariableNames', paramNames);
+            % Params = table({'B0'}, 3, {'TissueType'}, 'GM', {'M0a'}, [], {'Raobs'}, [], {'R'}, [], {'T2a'}, [], {'T1D'}, [], {'lineshape'}, ...
+            %     [], {'R1b'}, [], {'T2b'}, [], {'Ra'}, [], {'M0b'}, [], {'D'}, []);
+            % Params.B0 = 3;
+            % Params.TissueType = 'GM';
+            % Params = AI_defaultTissueParams(Params); 
+            % obj.Prot.DefaultTissueParams.Mat = Params;
+
+       % function tissueParams = getDefaultTissueParams(obj, paramsStruct)
+%     AI_defaultTissueParams(paramsStruct);
+%     tissueParams = obj.Prot.DefaultTissueParams.Mat(paramsStruct);
+% end 
