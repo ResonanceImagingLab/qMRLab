960691560cbaaf5688fbed1bec899b98346326b5
Amie Demmans <demmansa@mcmaster.ca>
2024-09-18 13:07:22 -0400

    removing ihMTsat correction and moving to other code

--- src/Models_Functions/ihMTfun/main_functions/ihMT_R1vsM0b_correlation.m
+++ src/Models_Functions/ihMTfun/main_functions/ihMT_R1vsM0b_correlation.m
@@ -1,4 +1,4 @@
-function ihMT_R1vsM0b_correlation(obj, data)
+function [fitValues_D, fitValues_SP, fitValues_SN] = ihMT_R1vsM0b_correlation(obj, data)
 
 % Sequence Simulations Results Directory 
 SeqSimDir = obj.options.R1vsM0bMapping_DataDirectory;
@@ -115,67 +115,67 @@ fitValues_SN = ihMT_generate_R1vsM0B_correlation( R1_s, M0b_neg, tempMask, fitVa
 %% Now use these results to B1 correct the data:
 %OutputDir = DATADIR;
 
-b1_1 = obj.Prot.PulseSequenceParams.Mat(8);
-
-corr_prot_d = MTsat_B1corr_factor_map(b1, R1_s, b1_1, fitValues_D);
-corr_prot_p = MTsat_B1corr_factor_map(b1, R1_s, b1_1, fitValues_SP);
-corr_prot_n = MTsat_B1corr_factor_map(b1, R1_s, b1_1, fitValues_SN);
-
-
-% Part 2, apply correction map
-sat_dual_c = (sat_dual + sat_dual.* corr_prot_d) .* mask1;
-sat_pos_c  = (sat_pos + sat_pos.* corr_prot_p) .* mask1;
-sat_neg_c  = (sat_neg + sat_neg.* corr_prot_n) .* mask1;
-ihmt_c      = sat_dual_c - (sat_pos_c + sat_neg_c)/2;
-
-
-ihmt_c = double(limitHandler(ihmt_c,0, 0.05));
-
-ihmt_c( ihmt_c >= 0.05) = 0;
-
-%% View results
-figure; imshow3Dfull(sat_dual1_c , [0 0.06], jet); 
-figure; imshow3Dfull(sat_pos1_c , [0 0.06], jet)
-figure; imshow3Dfull(sat_neg1_c , [0 0.06], jet); 
-
-figure; imshow3Dfull(ihmt_c , [0 0.06], jet)
-
-%% Other things, save if you want
-mkdir(strcat(OutputDir,'R1vsM0b_results') )
- 
-hdr.file_name = strcat(OutputDir,'R1vsM0b_results/MTsat_dual_1.mnc.gz'); minc_write(hdr.file_name, hdr, sat_dual_c);
-hdr.file_name = strcat(OutputDir,'R1vsM0b_results/MTsat_pos_1.mnc.gz'); minc_write(hdr.file_name, hdr, sat_pos_c);
-hdr.file_name = strcat(OutputDir,'R1vsM0b_results/MTsat_neg_1.mnc.gz'); minc_write(hdr.file_name, hdr, sat_neg_c);
-hdr.file_name = strcat(OutputDir,'R1vsM0b_results/ihMTsat_1.mnc.gz'); minc_write(hdr.file_name, hdr, ihmt_c);
-
-hdr.file_name = strcat(OutputDir,'R1vsM0b_results/b1.mnc.gz'); minc_write(hdr.file_name, hdr, b1);
-
-%% 
-
-ihmtSlice1 = squeeze( ihmt_c(:,126,:));
-
-figure; imagesc(ihmtSlice1); axis image;
-colormap(gray)
-caxis([0 0.03])
-hold on
-line([0,175], [165,166], 'Color', 'r');
-
-
-ihmtProf1 = ihmtSlice1(165,:);
-
-% normalize:
-ihmtProf1 = ihmtProf1/ max(ihmtProf1);
-
-figure
-plot(ihmtProf1,'LineWidth',2); 
-title('Line Profile (L-R)');
-xlim([15, 160])
-hold off
-xlabel('Voxel Index');
-ylabel('Relative ihMT_{sat}')
-ax = gca; ax.FontSize = 20; 
-set(gcf,'position',[10,400,1000,600])   
-
+% b1_1 = obj.Prot.PulseSequenceParams.Mat(8);
+% 
+% corr_prot_d = MTsat_B1corr_factor_map(b1, R1_s, b1_1, fitValues_D);
+% corr_prot_p = MTsat_B1corr_factor_map(b1, R1_s, b1_1, fitValues_SP);
+% corr_prot_n = MTsat_B1corr_factor_map(b1, R1_s, b1_1, fitValues_SN);
+% 
+% 
+% % Part 2, apply correction map
+% sat_dual_c = (sat_dual + sat_dual.* corr_prot_d) .* mask1;
+% sat_pos_c  = (sat_pos + sat_pos.* corr_prot_p) .* mask1;
+% sat_neg_c  = (sat_neg + sat_neg.* corr_prot_n) .* mask1;
+% ihmt_c      = sat_dual_c - (sat_pos_c + sat_neg_c)/2;
+% 
+% 
+% ihmt_c = double(limitHandler(ihmt_c,0, 0.05));
+% 
+% ihmt_c( ihmt_c >= 0.05) = 0;
+% 
+% %% View results
+% figure; imshow3Dfull(sat_dual1_c , [0 0.06], jet); 
+% figure; imshow3Dfull(sat_pos1_c , [0 0.06], jet)
+% figure; imshow3Dfull(sat_neg1_c , [0 0.06], jet); 
+% 
+% figure; imshow3Dfull(ihmt_c , [0 0.06], jet)
+% 
+% %% Other things, save if you want
+% mkdir(strcat(OutputDir,'R1vsM0b_results') )
+% 
+% hdr.file_name = strcat(OutputDir,'R1vsM0b_results/MTsat_dual_1.mnc.gz'); minc_write(hdr.file_name, hdr, sat_dual_c);
+% hdr.file_name = strcat(OutputDir,'R1vsM0b_results/MTsat_pos_1.mnc.gz'); minc_write(hdr.file_name, hdr, sat_pos_c);
+% hdr.file_name = strcat(OutputDir,'R1vsM0b_results/MTsat_neg_1.mnc.gz'); minc_write(hdr.file_name, hdr, sat_neg_c);
+% hdr.file_name = strcat(OutputDir,'R1vsM0b_results/ihMTsat_1.mnc.gz'); minc_write(hdr.file_name, hdr, ihmt_c);
+% 
+% hdr.file_name = strcat(OutputDir,'R1vsM0b_results/b1.mnc.gz'); minc_write(hdr.file_name, hdr, b1);
+% 
+% %% 
+% 
+% ihmtSlice1 = squeeze( ihmt_c(:,126,:));
+% 
+% figure; imagesc(ihmtSlice1); axis image;
+% colormap(gray)
+% caxis([0 0.03])
+% hold on
+% line([0,175], [165,166], 'Color', 'r');
+% 
+% 
+% ihmtProf1 = ihmtSlice1(165,:);
+% 
+% % normalize:
+% ihmtProf1 = ihmtProf1/ max(ihmtProf1);
+% 
+% figure
+% plot(ihmtProf1,'LineWidth',2); 
+% title('Line Profile (L-R)');
+% xlim([15, 160])
+% hold off
+% xlabel('Voxel Index');
+% ylabel('Relative ihMT_{sat}')
+% ax = gca; ax.FontSize = 20; 
+% set(gcf,'position',[10,400,1000,600])   
+% 
 
 
 
