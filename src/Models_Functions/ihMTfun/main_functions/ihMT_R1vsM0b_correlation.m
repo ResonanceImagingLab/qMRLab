3086c5bb87a6e59b9ed583345ef40e94f4467682
amie-demmans <demmansa@mcmaster.ca>
2024-11-07 14:07:03 -0500

    deleted comb_resid

--- src/Models_Functions/ihMTfun/main_functions/ihMT_R1vsM0b_correlation.m
+++ src/Models_Functions/ihMTfun/main_functions/ihMT_R1vsM0b_correlation.m
@@ -1,4 +1,4 @@
-function [fitValues_D, fitValues_SP, fitValues_SN] = ihMT_R1vsM0b_correlation(obj, data, fitValues_dual, fitValues_single)
+function [fitValues_Dual, fitValues_SP, fitValues_SN] = ihMT_R1vsM0b_correlation(obj, data, fitValues_dual, fitValues_single)
 
 % Sequence Simulations Results Directory 
 %SeqSimDir = obj.options.R1vsM0bMapping_DataDirectory;
@@ -60,27 +60,27 @@ M0b_pos = zeros(size(sat_dual));
 M0b_neg = zeros(size(sat_dual));
 
 % Speed up by doing only a few axial slices 
-axialStart = 126; % 65
-axialStop = axialStart+3;%115;
+% axialStart = 126; % 65
+% axialStop = axialStart+3;%115;
 %figure; imshow3Dfull(sat_dual(:,axialStart:axialStop,:) , [0 0.06], jet)
 
 disp('Code will take ~ 2 hours to run');
 tic %  ~ 2hrs to run 
 for i = 1:size(sat_dual,1) % went to 149
     
-    for j = axialStart:axialStop % 1:size(sat_dual,2) % for axial slices
+    for j = 1:size(sat_dual,2) % j = axialStart:axialStop % for axial slices
         for k =  1:size(sat_dual,3) % sagital slices  65
             
             if tempMask(i,j,k) > 0 %&& dual_s(i,j,k,3) > 0
                                 
-                 [M0b_dual(i,j,k), ~,  ~]  = ihMT_fit_M0b_v2( b1(i,j,k), R1_s(i,j,k), sat_dual(i,j,k), fitValues_dual);
-                 [M0b_pos(i,j,k),  ~,  ~]  = ihMT_fit_M0b_v2( b1(i,j,k), R1_s(i,j,k), sat_pos(i,j,k), fitValues_single);               
-                 [M0b_neg(i,j,k),  ~,  ~]  = ihMT_fit_M0b_v2( b1(i,j,k), R1_s(i,j,k), sat_neg(i,j,k), fitValues_single);
+                 [M0b_dual(i,j,k), ~]  = ihMT_fit_M0b_v2( b1(i,j,k), R1_s(i,j,k), sat_dual(i,j,k), fitValues_dual);
+                 [M0b_pos(i,j,k),  ~]  = ihMT_fit_M0b_v2( b1(i,j,k), R1_s(i,j,k), sat_pos(i,j,k), fitValues_single);               
+                 [M0b_neg(i,j,k),  ~]  = ihMT_fit_M0b_v2( b1(i,j,k), R1_s(i,j,k), sat_neg(i,j,k), fitValues_single);
                  
             end
         end
     end
-    disp(i/size(sat_dual,1) *100)
+    %disp(i/size(sat_dual,1) *100)
     toc 
 end
 
@@ -110,12 +110,12 @@ tempMask = bwareaopen(tempMask, 10000,6);
 tempMask = imerode(tempMask, strel('sphere',2));
 % figure; imshow3Dfullseg(M0b_dual, [0 0.15],tempMask)
 
-mkdir(fullfile(OutputDir,'figures'));
+mkdir(fullfile(OutputDir,'figures3'));
 
-%OutputDir = '/Users/amiedemmans/Documents/ihMT_Tests/Test3/';
+%OutputDir = '/Users/amiedemmans/Documents/ihMT_Tests/Test2/';
 
 % Optimized Approach
-fitValues_D  = ihMT_generate_R1vsM0B_correlation( R1_s, M0b_dual, tempMask, fitValues_dual, fullfile(OutputDir,'figures/R1vsM0b_dual.png'), fullfile(OutputDir,'fitValues_D.mat'));
+fitValues_Dual  = ihMT_generate_R1vsM0B_correlation( R1_s, M0b_dual, tempMask, fitValues_dual, fullfile(OutputDir,'figures/R1vsM0b_dual.png'), fullfile(OutputDir,'fitValues_Dual.mat'));
 fitValues_SP = ihMT_generate_R1vsM0B_correlation( R1_s, M0b_pos, tempMask, fitValues_single, fullfile(OutputDir,'figures/R1vsM0b_pos.png'), fullfile(OutputDir,'fitValues_SP.mat'));
 fitValues_SN = ihMT_generate_R1vsM0B_correlation( R1_s, M0b_neg, tempMask, fitValues_single, fullfile(OutputDir,'figures/R1vsM0b_neg.png'), fullfile(OutputDir,'fitValues_SN.mat'));
 
